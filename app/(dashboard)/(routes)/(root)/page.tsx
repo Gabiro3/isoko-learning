6fd0dd19ad6724f6b6ef9ec99311dce1ebd7fb6e
gabiroarnaud <gabiroarnaud@gmail.com>
2024-09-06 10:01:26 +0000

    Manually add course categories test

--- app/(dashboard)/(routes)/(root)/page.tsx
+++ app/(dashboard)/(routes)/(root)/page.tsx
@@ -4,7 +4,7 @@ import { CheckCircle, Clock } from 'lucide-react'
 import CoursesList from '@/components/course-list'
 import { getDashboardCourses } from '@/actions/get-dashboard-courses'
 import { InfoCard } from './_components/info-card'
-import { db} from "@/lib/db";
+import { db } from "@/lib/db";
 
 const insertDefaultCategories = async () => {
   const categories = ['Engineering', 'Music', 'Computer Science'];
@@ -24,7 +24,7 @@ export default async function Dashboard() {
   if (!userId) {
     return redirect('/')
   }
-  await insertDefaultCategories();
+  await insertDefaultCategories()
 
   const { completedCourses, coursesInProgress } = await getDashboardCourses(userId)
 
