b7c952dc91f9971049f63c767709376fcd14dec1
gabiroarnaud <gabiroarnaud@gmail.com>
2024-09-06 13:11:55 +0000

    Removed the purchase course button

--- app/(dashboard)/(routes)/(root)/page.tsx
+++ app/(dashboard)/(routes)/(root)/page.tsx
@@ -4,7 +4,6 @@ import { CheckCircle, Clock } from 'lucide-react'
 import CoursesList from '@/components/course-list'
 import { getDashboardCourses } from '@/actions/get-dashboard-courses'
 import { InfoCard } from './_components/info-card'
-//import { db } from '@/lib/db'
 
 
 export default async function Dashboard() {
