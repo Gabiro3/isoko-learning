3e2e7d5a977f585d8854ddd17b13e3227234870c
Gabiro IDX <gabiroarnaud@gmail.com>
2024-09-14 09:07:38 +0000

    Added Price formatting from USD to RWF

--- app/(dashboard)/(routes)/teacher/courses/_component/columns.tsx
+++ app/(dashboard)/(routes)/teacher/courses/_component/columns.tsx
@@ -34,9 +34,9 @@ export const columns: ColumnDef<Course>[] = [
     },
     cell: ({ row }) => {
       const price = parseFloat(row.getValue('price') || '0')
-      const formatted = new Intl.NumberFormat('en-US', {
+      const formatted = new Intl.NumberFormat('rw-RW', {
         style: 'currency',
-        currency: 'USD',
+        currency: 'RWF',
       }).format(price)
 
       return <div>{formatted}</div>
