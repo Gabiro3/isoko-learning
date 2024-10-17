099d3453cfb2351f75b80b7d15784fb61270c5dc
Gabiro IDX <gabiroarnaud@gmail.com>
2024-10-17 14:21:19 +0000

    Admin mode activate

--- components/navbar-routes.tsx
+++ components/navbar-routes.tsx
@@ -41,7 +41,7 @@ export const NavbarRoutes = () => {
         ) : isTeacher(userId) || isAdmin ? (
           <Link href="/teacher/courses">
             <Button size="sm" variant="ghost">
-              {isAdmin ? 'Admin mode' : 'Teacher mode'}
+              {(userId === ADMIN_ID) ? 'Admin mode' : 'Teacher mode'}
             </Button>
           </Link>
         ) : null}
