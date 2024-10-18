4de4fc8beeeef5e6037969ca06a8a472bbc55186
Gabiro IDX <gabiroarnaud@gmail.com>
2024-10-18 13:35:15 +0000

    admin rights

--- app/(dashboard)/(routes)/teacher/layout.tsx
+++ app/(dashboard)/(routes)/teacher/layout.tsx
@@ -1,6 +1,6 @@
 import { auth } from '@clerk/nextjs'
 import { redirect } from 'next/navigation'
-import { isAdminUser } from "@/lib/check-admin"
+import { isAdminUser } from '@/lib/check-admin'
 
 export default function TeacherLayout({ children }: { children: React.ReactNode }) {
   const { userId } = auth()
