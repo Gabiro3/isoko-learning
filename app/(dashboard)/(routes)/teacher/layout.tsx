227d37121f2b1ae2a31c25dc51f40f5724b3c174
Gabiro IDX <gabiroarnaud@gmail.com>
2024-10-25 13:01:08 +0000

    admin check

--- app/(dashboard)/(routes)/teacher/layout.tsx
+++ app/(dashboard)/(routes)/teacher/layout.tsx
@@ -2,11 +2,16 @@ import { auth } from '@clerk/nextjs'
 import { redirect } from 'next/navigation'
 import { isAdminUser } from '@/lib/check-admin'
 
-export default function ExpLayout({ children }: { children: React.ReactNode }) {
+export default async function ExpLayout({ children }: { children: React.ReactNode }) {
+  // Get the user ID and session data
   const { userId } = auth()
+
+  // Check if the user is an admin using your `isAdminUser` function
+  console.log(userId)
   if (!isAdminUser(userId)) {
     return redirect('/')
   }
 
   return <>{children}</>
 }
+
