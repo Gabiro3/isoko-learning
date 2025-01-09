15df5f1f443b25b3a337bade2933473a9eb112dc
gabiroarnaud <gabiroarnaud@gmail.com>
2025-01-09 08:30:21 +0000

    Added AI programming as a category and changed the isTeacher to check for a teacher id among a list of specified teacher ids

--- lib/teacher.ts
+++ lib/teacher.ts
@@ -1,4 +1,4 @@
 export function isTeacher(userId?: string | null): boolean {
-  const teacherIds = process.env.NEXT_PUBLIC_TEACHER_IDS?.split(',') || [];
-  return teacherIds.includes(userId || '');
+  const teacherIds = process.env.NEXT_PUBLIC_TEACHER_IDS?.split(',') || []
+  return teacherIds.includes(userId || '')
 }
