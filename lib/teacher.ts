a68ee5338905144301f3fe1c44532d65412352dc
gabiroarnaud <gabiroarnaud@gmail.com>
2025-01-09 08:43:50 +0000

    Added AI programming category and teacher ids list

--- lib/teacher.ts
+++ lib/teacher.ts
@@ -1,6 +1,4 @@
 export function isTeacher(userId?: string | null): boolean {
   const teacherIds = process.env.NEXT_PUBLIC_TEACHER_IDS?.split(',') || []
-  console.log(userId)
-  console.log(teacherIds)
   return teacherIds.includes(userId || '')
 }
